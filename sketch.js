50ca6339ec20ed1af471e54df8b856cad967ae11
rishabjain586 <73696911+rishabjain586@users.noreply.github.com>
2020-11-18 19:28:54 +0530

    car

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var car, wall;
+var speed, weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(55, 90);
+  weight = random(400, 1500);
+
+  car = createSprite(50, 200, 50, 50);
+  wall = createSprite(1500, 200, 60, height/2);
+  wall.shapeColor(80, 80, 80);
+
+}
+
+function draw() {
+  background(0);  
+
+  
+  
+  drawSprites();
+}
